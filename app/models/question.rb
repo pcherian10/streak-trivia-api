75278661c0760a7fe8749420733a6faed6163518
pcherian10 <pcherian10@gmail.com>
2019-05-19 18:58:07 -0400

    established relationships between models

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,4 @@
 class Question < ApplicationRecord
+    has_many :users, through: :attempts 
+    belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
+  has_many :questions
+  has_many :questions, through: :attempts 
+  has_many :streaks
+  
   has_secure_password
 end
