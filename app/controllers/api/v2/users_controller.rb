56c641a1d121bab696b2ac31b83dda00c176a794
pcherian10 <pcherian10@gmail.com>
2019-05-20 17:48:46 -0400

    removed first-name and last-name to simplify game play

--- app/controllers/api/v2/users_controller.rb
+++ app/controllers/api/v2/users_controller.rb
@@ -48,8 +48,6 @@ class Api::V2::UsersController < ApplicationController
       .permit(
         :username,
         :email,
-        :first_name,
-        :last_name,
         :password_digest
       )
     end
---
+++ db/migrate/20190520214210_remove_column_first_name_from_users.rb
@@ -0,0 +1,5 @@
+class RemoveColumnFirstNameFromUsers < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :users, :first_name
+  end
+end
